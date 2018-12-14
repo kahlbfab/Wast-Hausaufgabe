db98794116f17501da8998421ae3ce0e3f2e4dec
Jonas-Bra <44462551+Jonas-Bra@users.noreply.github.com>
2018-12-14 15:42:16 +0100

    A6 daten einlesen versuch 1

--- HA_main.R
+++ HA_main.R
@@ -148,24 +148,25 @@ ordered_PM10 <- left_join(x = select(luftqual, Datum,`Feinstaub PM10`, Station),
           by = "Datum")
   
 # Plot
-ggplot(ordered_PM10, aes(x= Regendauer, y= `Feinstaub PM10`)) + geom_point(aes(color = Station))
+ggplot(ordered_PM10, aes(x= Regendauer, y= `Feinstaub PM10`)) +
+  geom_point(aes(color = Station),  alpha = 0.5)
 
-
+# Test
+# stimmt das? => würde klar nicht verworfen (evt fehler wegen NA?)
+t.test(x = ordered_PM10$`Feinstaub PM10`[ordered_PM10$Regendauer == 0], 
+       y = ordered_PM10$`Feinstaub PM10`[ordered_PM10$Regendauer != 0],
+       conf.level = 0.99, alternative = "less")
 
 ##### Aufgabe 6#################
+# fehler beim einlesen weil bei gewissen Antworten kommas drin sind
+fuck_name <- c("intnr2015Sort","stadtkreiseLang","stadtkreiseSort","f10000Lang","f10000Sort","f20000Lang","f20000Sort","f20010Lang","f20010Sort","f21000_rec2aLang","f21000_rec2aSort","f21000_rec2bLang","f21000_rec2bSort","f21000_rec2cLang","f21000_rec2cSort","f23001Lang","f23001Sort","f23002Lang","f23002Sort","f23003Lang","f23003Sort","f23004Lang","f23004Sort","f23005Lang","f23005Sort","f23006Lang","f23006Sort","f23007Lang","f23007Sort","f23008Lang","f23008Sort","f23009Lang","f23009Sort","f23010Lang","f23010Sort","f23011Lang","f23011Sort","f23013Lang","f23013Sort","f23017Lang","f23017Sort","f23019Lang","f23019Sort","f24001Lang","f24001Sort","f24002Lang","f24002Sort","f24003Lang","f24003Sort","f24004Lang","f24004Sort","f24005Lang","f24005Sort","f24006Lang","f24006Sort","f24007Lang","f24007Sort","f24008Lang","f24008Sort","f24009Lang","f24009Sort","f24010Lang","f24010Sort","f24011Lang","f24011Sort","f24012Lang","f24012Sort","f24013Lang","f24013Sort","f24014Lang","f24014Sort","f25000Lang","f25000Sort","f25050Lang","f25050Sort","f25060Lang","f25060Sort","f25070Lang","f25070Sort","f25080Lang","f25080Sort","f25100Lang","f25100Sort","f25200Lang","f25200Sort","f25300a_recLang","f25300a_recSort","f25300b_recLang","f25300b_recSort","f25300c_recLang","f25300c_recSort","f25300d_recLang","f25300d_recSort","f26010Lang","f26010Sort","f26020Lang","f26020Sort","f26030Lang","f26030Sort","f26050Lang","f26050Sort","f26060Lang","f26060Sort","f30210Lang","f30210Sort","f30700Lang","f30700Sort","f30800Lang","f30800Sort","f31000Lang","f31000Sort","f35000Lang","f35000Sort","f35100Lang","f35100Sort","f36101Lang","f36101Sort","f36102Lang","f36102Sort","f36103Lang","f36103Sort","f36104Lang","f36104Sort","f36105Lang","f36105Sort","f36106Lang","f36106Sort","f36107Lang","f36107Sort","f36108Lang","f36108Sort","f36109Lang","f36109Sort","f36110Lang","f36110Sort","f36200Lang","f36200Sort","f36400a_recLang","f36400a_recSort","f36400b_recLang","f36400b_recSort","f36400c_recLang","f36400c_recSort","f40100Lang","f40100Sort","f40200Lang","f40200Sort","f40300Lang","f40300Sort","f40400a_recLang","f40400a_recSort","f40400b_recLang","f40400b_recSort","f40400c_recLang","f40400c_recSort","f40400d_recLang","f40400d_recSort","f40400e_recLang","f40400e_recSort","f40400f_recLang","f40400f_recSort","f40400g_recLang","f40400g_recSort","f50001Lang","f50001Sort","f50002Lang","f50002Sort","f50003Lang","f50003Sort","f50004Lang","f50004Sort","f50005Lang","f50005Sort","f50030Lang","f50030Sort","f50040Lang","f50040Sort","f50051Lang","f50051Sort","f50052Lang","f50052Sort","f50053Lang","f50053Sort","f60010Lang","f60010Sort","f60023Lang","f60023Sort","f60025Lang","f60025Sort","f60026Lang","f60026Sort","f60027Lang","f60027Sort","f60028Lang","f60028Sort","f60100Lang","f60100Sort","f60110Lang","f60110Sort","f60121Lang","f60121Sort","f60123Lang","f60123Sort","f60124Lang","f60124Sort","f60125Lang","f60125Sort","f60126Lang","f60126Sort","f60200Lang","f60200Sort","f60210Lang","f60210Sort","f60221Lang","f60221Sort","f60223Lang","f60223Sort","f60224Lang","f60224Sort","f60225Lang","f60225Sort","f60227Lang","f60227Sort","f60300Lang","f60300Sort","f60310Lang","f60310Sort","f60322Lang","f60322Sort","f60324Lang","f60324Sort","f60325Lang","f60325Sort","f60327Lang","f60327Sort","f60328Lang","f60328Sort","f60500Lang","f60500Sort","f72000Lang","f72000Sort","gewichtSort")
+bev_bef <- read_delim("bevoelkerungsbefragung_2015_stadtentwicklung_zuerich.csv",
+                                            quote = '"', delim = ',', skip = 1, col_names = fuck_name,
+                                            locale = locale(encoding = "latin1"))
+View(bev_bef)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+names(bev_bef)[1] <- "Nummer"
+num_change <- seq(0,242,2)
+num_change
 
 
